46d6f4ce6d3ab875b8d0d82dd10d668e40dd33bb
michaeltetteh <miketetteh751@outlook.com>
2020-04-30 17:03:24 +0000

    generated chart and line_item scaffolds

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+    has_many  :line_items
+    before_destroy  :ensure_not_referenced_by_any_line_item
+
+
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: {greater_than_or_equals_to: 0.01}
     validates :title, uniqueness: true 
@@ -8,5 +12,14 @@ class Product < ApplicationRecord
     }  
     # validates :image_url, allow_blank: false
     
-    validates :title, length: {minimum:10} 
+    validates :title, length: {minimum:10}
+    
+    private
+      def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, 'Line Items present')
+            throw :abort
+        end
+      end
+
 end
