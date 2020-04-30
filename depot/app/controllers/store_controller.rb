4eaabe583bb4539d78103796f238b766eb05f836
michaeltetteh <miketetteh751@outlook.com>
2020-04-30 21:54:41 +0000

    .

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  include CurrentCart
+
+  before_action :sess_counter, only: [:index]
+
   def index
     @products = Product.order(:title)
   end
 end
+
