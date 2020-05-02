3605843ec775019eb80f868dc71a20d1f3a83467
michaeltetteh <miketetteh751@outlook.com>
2020-05-02 00:17:51 +0000

    combine items migration

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_233129) do
+ActiveRecord::Schema.define(version: 2020_05_01_235925) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
